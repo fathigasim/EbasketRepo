ba6b5c4b39574e885487318b8651dca1269848e3
[Mohammed Fathi] <[test@email.com]>
2026-01-04 19:02:43 +0200

    Adding test controller

---
+++ Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SecureApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+
+        public TestController()
+        {
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> Data()
+        {
+            var data = new List<string> { "one", "two", "three" };
+            return Ok(data);
+        }
+    }
+}
